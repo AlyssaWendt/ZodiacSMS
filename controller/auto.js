73e61edeb9f156529d5fce1ab0e62543709db034
Alyssa Wendt <alyssa95wendt@gmail.com>
2022-10-16 19:01:50 -0400

    make it better

--- controller/auto.js
+++ controller/auto.js
@@ -4,6 +4,8 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const axios = require("axios");
+require("dotenv").config();
+
 
 
 async function auto() {
